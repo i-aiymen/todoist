964404a2cc44283667fc030cbb09d22518f20f64
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2022-02-11 11:23:32 +0530

    Updated: Add task screen page UI

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -5,7 +5,11 @@ class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
+      physics: BouncingScrollPhysics(),
+      scrollDirection: Axis.vertical,
       children: [
+        TaskTile(),
+        TaskTile(),
         TaskTile(),
         TaskTile(),
       ],
