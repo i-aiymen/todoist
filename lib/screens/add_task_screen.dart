3c0a9b0628734ad09f13585b2a66df999614545c
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2022-02-11 23:19:49 +0530

    Updated: Adding tasks using provider

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoist/models/task.dart';
+import 'package:todoist/models/task_data.dart';
 
 class AddTaskScreen extends StatelessWidget {
-  final Function addTaskCallback;
-
-  AddTaskScreen(this.addTaskCallback);
-
   @override
   Widget build(BuildContext context) {
     String newTaskTitle = '';
@@ -39,8 +38,9 @@ class AddTaskScreen extends StatelessWidget {
           ),
           FlatButton(
             onPressed: () {
-              addTaskCallback(newTaskTitle);
-              //print(newTaskTitle);
+              Provider.of<TaskData>(context, listen: false)
+                  .addTask(newTaskTitle);
+              Navigator.pop(context);
             },
             child: Text(
               'Add',
