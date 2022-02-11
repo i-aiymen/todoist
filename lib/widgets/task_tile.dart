26b4b8fd090a80a1830870df466c54b2b5f81c30
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2022-02-11 23:46:30 +0530

    Updating: Deleting a task on long pressing

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -4,12 +4,18 @@ class TaskTile extends StatelessWidget {
   final bool isChecked;
   final String taskTitle;
   final void Function(bool?) checkboxCallback;
+  final VoidCallback longPressCallback;
 
-  TaskTile({required this.taskTitle,required this.checkboxCallback,required this.isChecked});
+  TaskTile(
+      {required this.taskTitle,
+      required this.checkboxCallback,
+      required this.isChecked,
+      required this.longPressCallback});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onLongPress: longPressCallback,
       title: Text(
         taskTitle,
         style: TextStyle(
